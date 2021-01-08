caca90cadaa4078e0f961b77ca53bf477ba74062
ramcharanthota <icherryme.0407@gmail.com>
2021-01-08 03:08:31 +0000

    test_slam updated

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -2,7 +2,7 @@
 
 xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find gmapping)/../../map/myhome_robot.world"&
 sleep 5
-xterm  -e  " roslaunch gmapping gmapping_demo.launch" & 
+xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch" & 
 sleep 5
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
