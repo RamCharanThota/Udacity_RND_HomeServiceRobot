7fdd2fd3cb77fc0c03cd6b8a2dff6bd3f5dc5464
ramcharanthota <icherryme.0407@gmail.com>
2021-01-09 07:19:21 +0000

    imperfecr map

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find gmapping)/../../map/myhome_robot.world"&
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find gmapping)/../../map/my_tbuild_home.world"&
 sleep 5
-xterm  -e  " roslaunch gmapping amcl_demo.launch" & 
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch" & 
 sleep 5
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" 
 
--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find gmapping)/../../map/myhome_robot.world"&
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find gmapping)/../../map/my_home_prop.world"&
 sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch" & 
 sleep 5
