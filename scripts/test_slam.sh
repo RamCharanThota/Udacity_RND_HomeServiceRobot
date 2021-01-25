0589c8b854cca81faf81dd4f33611d655d0ea397
ramcharanthota <icherryme.0407@gmail.com>
2021-01-25 04:25:04 +0000

    gazebo world file fixed

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find gmapping)/../../map/final_home.world"&
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find gmapping)/../../map/final_updated_home.world"&
 sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch" & 
 sleep 5
