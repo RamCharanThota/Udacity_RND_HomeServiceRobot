a0ea767f06077b7164762abbea3d789874b1a292
ramcharanthota <icherryme.0407@gmail.com>
2021-01-28 02:08:25 +0000

    navigation task completed

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -4,7 +4,10 @@ xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(ros
 sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch" & 
 sleep 5
-xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" 
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm  -e  " rosrun pick_objects pick_objects"
+
 
 
 
